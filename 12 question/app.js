9af09ed393f2728f73b592fb3e03a1fe45ef60a5
Maryam Saleem <161303403+MaryamSaleem000@users.noreply.github.com>
2024-07-31 12:00:58 -0700

    Add files via upload

---
+++ 12 question/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let Names = ["Maryam", "Hina", "Fatima", "Fiza", "Kousar"];
+console.log(`Hello ${Names[0]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[1]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[2]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[3]}, would you like to learn some typescript today? `);
+console.log(`Hello ${Names[4]}, would you like to learn some typescript today? `);
